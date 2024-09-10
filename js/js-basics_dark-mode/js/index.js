77a751781007083b59c9a28ecb4f8930b68d442b
Ibo Ndure <orfanos@hotmail.de>
2024-09-10 16:06:20 +0200

    changes

---
+++ js/js-basics_dark-mode/js/index.js
@@ -0,0 +1,11 @@
+console.clear();
+
+const bodyElement = document.querySelector('[data-js="body"]');
+addEventListener;
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  console.log("Yeah");
+});
