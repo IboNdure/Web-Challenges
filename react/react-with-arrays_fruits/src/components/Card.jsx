ab72239ed6e5ff72d350cfc4d5d20aa50aa60830
Ibo Ndure <orfanos@hotmail.de>
2024-10-02 15:34:06 +0200

    addreactbonus

--- react/react-with-arrays_fruits/src/components/Card.jsx
+++ react/react-with-arrays_fruits/src/components/Card.jsx
@@ -1,10 +1,13 @@
 import "./Card.css";
 
 export default function Card({ name, color }) {
+  const cardstyle = {
+    backgroundColor: color,
+  };
+
   return (
-    <div className="card">
+    <div className="card" style={cardstyle}>
       <p>{name}</p>
-      <p>Color: {color}</p> {/* Display the color here */}
     </div>
   );
 }
